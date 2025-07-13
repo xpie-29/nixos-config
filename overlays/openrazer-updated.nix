6d5d6c6274af883887e23f069fd6070cf7636491
xpie-29 <xpie@proton.me>
2025-07-13 01:10:55 -0400

    fixed openrazer

--- overlays/openrazer-updated.nix
+++ overlays/openrazer-updated.nix
@@ -1,13 +1,13 @@
 final: prev: {
   openrazer = prev.openrazer.overrideAttrs (old: {
-    pname = "openrazer-git";
-    version = "unstable-git";
+    pname = "openrazer";
+    version = "3.7.0";
 
     src = prev.fetchFromGitHub {
       owner = "openrazer";
       repo = "openrazer";
-      rev = "267c5ec3c4dfac2fddc50e445b1a77e223d7091c";  # known working post-hrtimer fix
-      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";  # you'll fix this below
+      rev = "v3.7.0";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";  # placeholder
     };
   });
 }
