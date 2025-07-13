5583bcf4393f54b4a5f6d3153bd6313790772908
xpie-29 <xpie@proton.me>
2025-07-13 09:33:24 -0400

    add kitty and oh-my-zsh

--- home/xpie.nix
+++ home/xpie.nix
@@ -7,7 +7,34 @@
 
   programs.home-manager.enable = true;
 
-  programs.zsh.enable = true;
+# ZSHELL
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "jonathan";
+      plugins = [ "git" "z" "extract" ];
+    };
+  };
+
+  # STARSHIP
+
+  # KITTY
+  programs.kitty = {
+    enable = true;
+    font.name = "JetBrainsMono Nerd Font Mono";
+    font.size = 12;
+    theme = "Nordfox";
+    settings = {
+      scrollback_lines = 10000;
+      confirm_os_window_close = 0;
+      dynamic_background_opacity = true;
+      enable_audio_bell = false;
+      window_padding_width = 6;
+      background_opacity = "0.9";
+      background_blur = 3;
+    };
+  };
 
   home.packages = with pkgs; [
 
