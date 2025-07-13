f9c0e6297f918367227b357cbeb8b0a792de0fb0
xpie-29 <xpie@proton.me>
2025-07-13 16:47:39 -0400

    added nix extensions for vscode

--- flake.nix
+++ flake.nix
@@ -2,9 +2,14 @@
   description = "NixOS with flakes and home-manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  #  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    alejandra.url = "github:kamadorueda/alejandra/4.0.0";
+    alejandra.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -18,7 +23,6 @@
           home-manager.useGlobalPkgs = true;
           home-manager.users.xpie = import ./home/xpie.nix;
           home-manager.backupFileExtension = "bak";
-
         }
       ];
     };
